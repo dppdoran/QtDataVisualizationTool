e88f08afa8069c7220d2ea3eb03993a2d6d78229
Dermot Doran <dppdoran@outlook.com>
2019-10-23 11:48:33 +0200

    Third step: Creating an empty QMainWindow

--- earthquake.py
+++ earthquake.py
@@ -1,10 +1,12 @@
+import sys
 import argparse
 import pandas as pd
-from PySide2 import QtCore
+from PySide2.QtWidgets import QApplication, QMainWindow, QAction
+from PySide2.QtCore import QDateTime, QTimeZone, Slot
 
 def transform_date(utc, timezone=None):
     utc_format = "yyyy-MM-ddTHH:mm:ss.zzzZ"
-    new_date = QtCore.QDateTime().fromString(utc, utc_format)
+    new_date = QDateTime().fromString(utc, utc_format)
     if timezone:
         new_date.setTimeZone(timezone)
     return new_date
@@ -18,11 +20,33 @@ def read_data(fname):
     magnitudes = df["mag"]
 
     # Local timezone
-    timezone = QtCore.QTimeZone(b"Europe/Berlin")
+    timezone = QTimeZone(b"Europe/Berlin")
 
     # Get timestamps transformed to local timezone
     times = df["time"].apply(lambda x: transform_date(x, timezone))
     return times, magnitudes
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Earthquakes information")
+        # Setup menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
+        exit_action = QAction("Exit", self)
+        exit_action.setShortcut("Ctrl+Q")
+        exit_action.triggered.connect(self.exit_app)
+        self.file_menu.addAction(exit_action)
+        self.status = self.statusBar()
+        self.status.showMessage('Data loaded and plotted.')
+
+        geometry = app.desktop().availableGeometry(self)
+        self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
+
+    @Slot()
+    def exit_app(self, checked):
+        sys.exit()
 
 
 if __name__ == '__main__':
@@ -31,3 +55,7 @@ if __name__ == '__main__':
     args = options.parse_args()
     data = read_data(args.file)
     print(data)
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
